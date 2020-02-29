deb52f8cb1b3bf69e3716196f237ae20bcd6646b
Enes Tasbasi <enestasbasi@outlook.com>
2020-02-29 08:46:41 -0500

    Create the navbar and landing image/logo
    Fork the static webpack template. Add the navbar and the placeholder
    landing image.

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,21 @@
-if (module.hot) {
-  module.hot.accept();
-}
+// if (module.hot) {
+//   module.hot.accept();
+// }
+
+let $hamburgerMenu = document.querySelector('.hamburger-menu');
+let $dropdownMenu = document.querySelector('.dropdown-menu');
+
+let menuIsOpen = false;
+
+$hamburgerMenu.addEventListener('click', () => {
+
+  if (!menuIsOpen) {
+    menuIsOpen = true;
+    $dropdownMenu.classList.add('open');
+    $hamburgerMenu.classList.add('open');
+  } else {
+    menuIsOpen = false;
+    $dropdownMenu.classList.remove('open');
+    $hamburgerMenu.classList.remove('open');
+  }
+});
